170485e60a3f65822a8f065e04674928347788c4
anaeremina <eremina@bu.edu>
2022-11-24 11:46:55 -0500

    Changes: Ana

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,31 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
 
+/* TIMER */
+let timeLeft = 4;
+let timer = document.getElementById('timeLeft');
+
+function isTimeLeft() {
+  return timeLeft > -1;
+}
+
+function runTimer(timerElement) {
+	const timerCircle = timerElement.querySelector('svg > circle + circle');
+	timerElement.classList.add('animatable');
+	timerCircle.style.strokeDashoffset = 1;
+    
+	let countdownTimer = setInterval(function(){
+		if(isTimeLeft()){
+			const timeRemaining = timeLeft--;
+			const normalizedTime = (4 - timeRemaining) / 4;
+      // for clockwise animation
+      // const normalizedTime = (timeRemaining - 60) / 60;
+			timerCircle.style.strokeDashoffset = normalizedTime;
+      timer.innerHTML = timeRemaining;
+		} else {
+			clearInterval(countdownTimer);
+			timerElement.classList.remove('animatable');
+		}  
+	}, 1000);
+}
+
+runTimer(document.querySelector('.timer'));
