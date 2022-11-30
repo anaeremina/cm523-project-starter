ca9194222bac42f062ef24128d0c8aa05a6d3a45
anaeremina <eremina@bu.edu>
2022-11-30 15:05:25 -0500

    Changes:Ana

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,27 +31,8 @@ function runTimer(timerElement) {
 runTimer(document.querySelector('.timer'));
 
 
-/* CALENDAR */
-$(document).ready(function() {
+/* CALENDAR */ 
 
-    // page is now ready, initialize the calendar...
-
-  $('#calendar').fullCalendar({
-    defaultView: 'month', //Possible Values: month, basicWeek, basicDay, agendaWeek, agendaDay
-    header: { 
-      left:   'title',
-      center: '',
-      right:  'today prevYear,prev,next,nextYear', //Possible Values: month, basicWeek, basicDay, agendaWeek, agendaDay, today prevYear,prev,next,nextYear
-    },
-    buttonIcons :{
-      prev: 'left-single-arrow',
-      next: 'right-single-arrow',
-      prevYear: 'left-double-arrow',
-      nextYear: 'right-double-arrow'
-    }
-    });
-
-});
 
 
 /* TASKS */
